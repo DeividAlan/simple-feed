ca51c2994cbfe2211f5b87e802effcdb66a1c74c
Deivid Alan <deividalan@hotmail.com>
2023-06-26 18:14:26 -0300

    Initial commit

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,8 @@
+export function Post() {
+  return (
+    <>
+      <strong>Deivid</strong>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, ipsum dolorum. Laudantium eaque totam dolorem laboriosam delectus. Explicabo perferendis accusamus impedit dignissimos optio, iure quia maiores pariatur exercitationem officiis rerum!</p>
+    </>
+  );
+}
